914d8e9b4d1702166330b6cdcd26e8fc51a8b75e
Peter Gledhill <petergledhill@gmail.com>
2012-02-07 14:27:14 +0000

    removed dodgy code

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -23,15 +23,15 @@ class WebhookController < ApplicationController
 
   private
   
-  def verify_webhook
-    data = request.body.read.to_s
-    hmac_header = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
-    digest  = OpenSSL::Digest::Digest.new('sha256')
-    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, Give:Application.config.shopify.secret, data)).strip
-    unless calculated_hmac == hmac_header
-      head :unauthorized
-    end
-    request.body.rewind
-  end
+  # def verify_webhook
+    # data = request.body.read.to_s
+    # hmac_header = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
+    # digest  = OpenSSL::Digest::Digest.new('sha256')
+    # calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, Give:Application.config.shopify.secret, data)).strip
+    # unless calculated_hmac == hmac_header
+      # head :unauthorized
+    # end
+    # request.body.rewind
+  # end
 
 end
