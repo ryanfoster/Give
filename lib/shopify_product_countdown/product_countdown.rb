a7975e0bb4a2c7fc25143936a285073eb3fab561
Peter Gledhill <petergledhill@gmail.com>
2012-03-14 11:01:19 +0000

    product can now have no countdown data

--- lib/shopify_product_countdown/product_countdown.rb
+++ lib/shopify_product_countdown/product_countdown.rb
@@ -4,25 +4,32 @@ class ProductCountdown
     
      begin
       start_date, end_date = ProductTagParser.extract_dates(product.tags)    
+      start_date += 0.seconds
+      end_date += (1.day - 1.seconds)
      rescue
        # problem getting date? assume it was in the past
-      start_date = Date.today - 2.days
-      end_date = Date.today - 1.days
+      start_date = nil
+      end_date = nil
      end
-     
-     start_date += 0.seconds
-     end_date += (1.day - 1.seconds)
-     
+          
     set_countdown(product, start_date, end_date)
     
   end
   
   def self.set_countdown(product, start_date, end_date)
-    set_meta(product, "begin_date_secs", start_date.to_i, "integer")
-    set_meta(product, "end_date_secs", end_date.to_i, "integer")
-       
-    set_meta(product, "begin_date", start_date.strftime("%a, %d %b %Y %H:%M:%S %Z"), "string")
-    set_meta(product, "end_date", end_date.strftime("%a, %d %b %Y %H:%M:%S %Z"), "string")
+    start_string = ""
+    end_string = ""
+    
+    if start_date && end_date
+      start_string = start_date.strftime("%a, %d %b %Y %H:%M:%S %Z")
+      end_string = end_date.strftime("%a, %d %b %Y %H:%M:%S %Z")
+    end
+    
+    set_meta(product, "begin_date_secs", start_date ? start_date.to_i : 0, "integer")
+    set_meta(product, "end_date_secs", end_date ? end_date.to_i : 0, "integer")
+           
+    set_meta(product, "begin_date", start_string, "string")
+    set_meta(product, "end_date", end_string, "string")
   end
     
   def self.get_meta( product, key )
